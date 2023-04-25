3a8769a11d7510c8986aae8e5a0ee23a5cf2396b
Renan Kovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-04-25 20:35:27 -0300

    update main

--- main.py
+++ main.py
@@ -27,65 +27,79 @@ def gerenateWeather():
         weather_window.geometry("390x844")
         weather_window.config(bg='#0B2447')
 
-        hello_msg = tk.Label(weather_window, text="Hello!", font=('Arial', 32), bg='#0B2447', fg='white')
-        hello_msg.place(x=10, y=10)
+        hello_msg = tk.Label(weather_window, text="Hello!", font=('Arial', 32, "bold"), bg='#0B2447', fg='white')
+        hello_msg.place(x=21, y=30)
 
         check_weather = tk.Label(weather_window, text="Check weather location", font=('Arial', 12), bg='#0B2447',
-                                 fg='white')
-        check_weather.place(x=10, y=55)
+                                 fg='#206BD4')
+        check_weather.place(x=21, y=73)
 
-        temperature_label = tk.Label(weather_window, text=f'Weather: {round(temperature - 273.15, 2)} ºC', font=("Arial"
-                                                                                                                 , 20))
-        temperature_label.pack()
+        city_name = tk.Label(weather_window, text=f"{CITY}", font=('Arial', 22, "bold"), bg='#0B2447', fg='white')
+        city_name.place(x=139, y=140)
 
-        humidity_label = tk.Label(weather_window, text=f'Humidity: {humidity}%', font=("Arial", 20))
-        humidity_label.pack()
+        temperature_label = tk.Label(weather_window, text=f'{round(temperature - 273.15)} ', font=("Arial", 72),
+                                     bg='#0B2447', fg='white')
+        temperature_label.place(x=128, y=180)
 
-        pressure_label = tk.Label(weather_window, text=f'Pressure: {pressure/1000} KPa', font=("Arial", 20))
-        pressure_label.pack()
+        celsius_label = tk.Label(weather_window, text='ºC', font=('Arial', 12), bg='#0B2447', fg='white')
+        celsius_label.place(x=252, y=200)
 
-        weather_report_label = tk.Label(weather_window, text=f'Dresciption: {report[0]["description"]}', font=("Arial",
-                                                                                                               20))
-        weather_report_label.pack()
+        description = tk.Label(weather_window, text=f'{report[0]["description"]}', font=("Arial", 14), bg='#0B2447',
+                               fg='white')
+        description.place(x=125, y=298)
 
-        print(f"{CITY:-^30}")
-        print(f"Temperature: {round(temperature - 273.15, 2)} ºC")
-        print(f"Humidity: {humidity}%")
-        print(f"Pressure: {pressure / 1000} KPa")
-        print(f"Weather Report: {report[0]['description']}")
+        temperature_sentence = tk.Label(weather_window, text="Temperature:", font=("Arial", 20, "bold"), bg='#164B94',
+                                        fg='white')
+        temperature_sentence.place(x=38, y=462)
+
+        temperature_number = tk.Label(weather_window, text=f"{round(temperature-273.15)}ºC", font=("Arial", 20, "bold"),
+                                      bg='#164B94', fg='white')
+        temperature_number.place(x=259, y=464)
+
+        humidity_sentence = tk.Label(weather_window, text="Humidity:", font=("Arial", 20, "bold"), bg='#164B94',
+                                     fg='white')
+        humidity_sentence.place(x=38, y=520)
+
+        humidity_number = tk.Label(weather_window, text=f"{humidity}%", font=("Arial", 20, "bold"), bg='#164B94',
+                                   fg='white')
+        humidity_number.place(x=259, y=522)
+
+        pressure_sentence = tk.Label(weather_window, text="Pressure:", font=("Arial", 20, "bold"), bg='#164B94',
+                                     fg='white')
+        pressure_sentence.place(x=38, y=578)
+
+        pressure_number = tk.Label(weather_window, text=f"{pressure/1000} KPA", font=("Arial", 20, "bold"),
+                                   bg='#164B94', fg='white')
+        pressure_number.place(x=232, y=580)
+
+        def close_window():
+            weather_window.destroy()
+
+        close_button = tk.Button(weather_window, text="Close", bg='#081A33', fg='white', font=('Arial', 32),
+                                 command=close_window)
+        close_button.config(border=0, bd=0, highlightthickness=0, relief='flat')
+        close_button.configure(highlightbackground='#081A33', highlightcolor='#081A33')
+
+        close_button.place(x=128, y=752)
+
     else:
         print("Error in the HTTP request")
 
 
 if __name__ == "__main__":
-    # window = Tk()
-    # window.title("Weather App")
-    # window.config(padx=10, pady=100, bg='orange')
-
-    # label
-    # website_label = Label(text="Cidade:", bg='orange')
-    # website_label.grid(row=2, column=0)
-
-    # Entries
-    # website_entry = Entry(width=35)
-    # website_entry.grid(row=2, column=1, columnspan=2)
-    # website_entry.focus()
-    # add_button = Button(text="Condições Climáticas", width=30, height=1, command=gerenateWeather)
-    # add_button.grid(row=4, column=1, columnspan=2)
-    # add_button.place(x=25, y=50)
-
-    # window.mainloop()
     window = tk.Tk()
     window.title("Weather App")
-    window.geometry("220x200+10+20")
+    window.geometry("390x844")
+    window.config(bg='#0B2447')
 
-    city_label = tk.Label(window, text="City name: ")
-    city_label.place(x=10, y=50)
+    city_label = tk.Label(window, text="City name: ", bg='#0B2447', fg='white', font=('Arial', 32, "bold"))
+    city_label.place(x=79, y=133)
 
-    city_entry = tk.Entry(window)
-    city_entry.place(x=80, y=51)
+    city_entry = tk.Entry(window, bg='#0B2447', fg='white', font=('Arial', 22, "bold"))
+    city_entry.place(x=35, y=336)
 
-    get_weather_button = tk.Button(window, text="Get Weather", command=gerenateWeather)
-    get_weather_button.place(x=75, y=100)
+    get_weather_button = tk.Button(window, text="Get Weather", bg='#0B2447', fg='white', command=gerenateWeather,
+                                   font=('Arial', 28, "bold"))
+    get_weather_button.place(x=71, y=574)
 
     window.mainloop()
